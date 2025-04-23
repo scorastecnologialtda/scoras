2d627fbbfb23f88b606392deaaa69dedb654c7b2
Anderson <anderson@scoras.com.br>
2025-04-23 17:07:37 -0300

    Atualizando

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
     name="scoras",
-    version="0.2.3",
+    version="0.3.0",  # Updated version
     author="Anderson L. Amaral",
     author_email="luis.anderson.sp@gmail.com",
     description="Intelligent Agent Framework with Complexity Scoring",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/scorastecnologialtda/scoras",
-    packages=["scoras"],
+    packages=find_namespace_packages(include=["scoras", "scoras.*"]) ,  # This includes all subpackages
     include_package_data=True,
     python_requires=">=3.8",
     install_requires=[
